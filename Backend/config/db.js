092fde6e98ec47ad2c3580bde5b60a1fc4a8b67d
Faizankhan99 <usmani.faizan98@gmail.com>
2022-12-26 14:22:24 +0530

    adbackend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const connect = () => {
   return mongoose.connect(process.env.MONGOURL);
 };
